25f3d56055fe343b22a07f5ee23802c5cca08aa5
Sanin <sanin_andrey@mail.ru>
2019-12-09 12:33:18 +0700

    commit

--- TaurusTest.py
+++ TaurusTest.py
@@ -2,12 +2,13 @@ import sys
 from taurus.external.qt import Qt
 from taurus.qt.qtgui.application import TaurusApplication
 
+from taurus.qt.qtgui.display import TaurusLabel
+
 app = TaurusApplication(sys.argv, cmd_line_parser=None,)
 panel = Qt.QWidget()
 layout = Qt.QHBoxLayout()
 panel.setLayout(layout)
 
-from taurus.qt.qtgui.display import TaurusLabel
 w = TaurusLabel()
 layout.addWidget(w)
 w.model = 'ET7000_server/test/1/ai00'
